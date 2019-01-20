6f6fd4138e065e9ab9590d138df805d73c484b88
Eric Jonathan <eric-i@sellfazz.com>
2019-01-20 15:56:15 +0700

    finished template wip matrix letter

--- framebuffer.c
+++ framebuffer.c
@@ -6,6 +6,7 @@
 #include <sys/mman.h>
 #include <sys/ioctl.h>
 #include <time.h>
+#include <string.h>
 
 void writeCharacter(char *fbp, struct fb_var_screeninfo vinfo, struct fb_fix_screeninfo finfo, int i, int xoffset, int yoffset, int letter[10][10]);
 void writeCharacter(char *fbp, struct fb_var_screeninfo vinfo, struct fb_fix_screeninfo finfo, int i, int xoffset, int yoffset, int letter[10][10]) {
@@ -48,6 +49,7 @@ void writeCharacter(char *fbp, struct fb_var_screeninfo vinfo, struct fb_fix_scr
     }
 }
 
+
 int main()
 {
     int fbfd = 0;
@@ -57,8 +59,8 @@ int main()
     char *fbp = 0;
     int x = 0, y = 0;
     long int location = 0;
-              
-    int Q[10][10]={{0,0,0,0,0,0,0,0,0,0},
+
+    int A[10][10]={{0,0,0,0,0,0,0,0,0,0},
                    {0,0,0,0,0,0,0,0,0,0},
                    {0,0,0,0,0,0,0,0,0,0},
                    {0,0,0,0,0,0,0,0,0,0},
@@ -68,6 +70,17 @@ int main()
                    {0,0,0,0,0,0,0,0,0,0},
                    {0,0,0,0,0,0,0,0,0,0},
                    {0,0,0,0,0,0,0,0,0,0}};
+
+    int B[10][10]={{0,0,0,0,0,0,0,0,0,0},
+                   {0,0,0,0,0,0,0,0,0,0},
+                   {0,0,0,0,0,0,0,0,0,0},
+                   {0,0,0,0,0,0,0,0,0,0},
+                   {0,0,0,0,0,0,0,0,0,0},
+                   {0,0,0,0,0,0,0,0,0,0},
+                   {0,0,0,0,0,0,0,0,0,0},
+                   {0,0,0,0,0,0,0,0,0,0},
+                   {0,0,0,0,0,0,0,0,0,0},
+                   {0,0,0,0,0,0,0,0,0,0}};    
 
     int C[10][10]={{0,0,1,1,1,1,1,1,1,0},
                    {0,1,1,1,1,1,1,1,1,0},
@@ -80,28 +93,6 @@ int main()
                    {0,1,1,1,1,1,1,1,1,0},
                    {0,0,1,1,1,1,1,1,1,0}};
     
-    int E[10][10]={{1,1,1,1,1,1,1,1,1,0},
-                   {1,1,1,1,1,1,1,1,1,0},
-                   {1,1,0,0,0,0,0,0,0,0},
-                   {1,1,0,0,0,0,0,0,0,0},
-                   {1,1,1,1,1,1,1,1,0,0},
-                   {1,1,1,1,1,1,1,1,0,0},
-                   {1,1,0,0,0,0,0,0,0,0},
-                   {1,1,0,0,0,0,0,0,0,0},
-                   {1,1,1,1,1,1,1,1,1,0},
-                   {1,1,1,1,1,1,1,1,1,0}};
-
-    int H[10][10]={{1,1,0,0,0,0,0,1,1,0},
-                   {1,1,0,0,0,0,0,1,1,0},
-                   {1,1,0,0,0,0,0,1,1,0},
-                   {1,1,0,0,0,0,0,1,1,0},
-                   {1,1,1,1,1,1,1,1,1,0},
-                   {1,1,1,1,1,1,1,1,1,0},
-                   {1,1,0,0,0,0,0,1,1,0},
-                   {1,1,0,0,0,0,0,1,1,0},
-                   {1,1,0,0,0,0,0,1,1,0},
-                   {1,1,0,0,0,0,0,1,1,0}};
-    
     int D[10][10]={{1,1,1,1,1,1,1,0,0,0},
                    {1,1,1,1,1,1,1,1,0,0},
                    {1,1,0,0,0,0,1,1,1,0},
@@ -113,16 +104,16 @@ int main()
                    {1,1,1,1,1,1,1,1,0,0},
                    {1,1,1,1,1,1,1,0,0,0}};
 
-    int P[10][10]={{1,1,1,1,1,1,1,0,0,0},
-                   {1,1,1,1,1,1,1,1,0,0},
-                   {1,1,0,0,0,0,0,1,1,0},
-                   {1,1,0,0,0,0,0,1,1,0},
-                   {1,1,0,0,0,0,0,1,1,0},
-                   {1,1,1,1,1,1,1,1,0,0},
-                   {1,1,1,1,1,1,1,0,0,0},
+    int E[10][10]={{1,1,1,1,1,1,1,1,1,0},
+                   {1,1,1,1,1,1,1,1,1,0},
                    {1,1,0,0,0,0,0,0,0,0},
                    {1,1,0,0,0,0,0,0,0,0},
-                   {1,1,0,0,0,0,0,0,0,0}};
+                   {1,1,1,1,1,1,1,1,0,0},
+                   {1,1,1,1,1,1,1,1,0,0},
+                   {1,1,0,0,0,0,0,0,0,0},
+                   {1,1,0,0,0,0,0,0,0,0},
+                   {1,1,1,1,1,1,1,1,1,0},
+                   {1,1,1,1,1,1,1,1,1,0}};
 
     int F[10][10]={{1,1,1,1,1,1,1,1,0,0},
                    {1,1,1,1,1,1,1,1,0,0},
@@ -135,7 +126,6 @@ int main()
                    {1,1,0,0,0,0,0,0,0,0},
                    {1,1,0,0,0,0,0,0,0,0}};    
 
-
     int G[10][10]={{1,1,1,1,1,1,1,1,0,0},
                    {1,1,1,1,1,1,1,1,0,0},
                    {1,1,0,0,0,0,0,0,0,0},
@@ -147,6 +137,17 @@ int main()
                    {1,1,1,1,1,1,1,1,0,0},
                    {1,1,1,1,1,1,1,1,0,0}};
 
+
+    int H[10][10]={{1,1,0,0,0,0,0,1,1,0},
+                   {1,1,0,0,0,0,0,1,1,0},
+                   {1,1,0,0,0,0,0,1,1,0},
+                   {1,1,0,0,0,0,0,1,1,0},
+                   {1,1,1,1,1,1,1,1,1,0},
+                   {1,1,1,1,1,1,1,1,1,0},
+                   {1,1,0,0,0,0,0,1,1,0},
+                   {1,1,0,0,0,0,0,1,1,0},
+                   {1,1,0,0,0,0,0,1,1,0},
+                   {1,1,0,0,0,0,0,1,1,0}};
 
     int I[10][10]={{1,1,1,1,1,1,1,1,0,0},
                    {1,1,1,1,1,1,1,1,0,0},
@@ -169,6 +170,189 @@ int main()
                    {1,1,0,0,0,1,1,0,0,0},
                    {1,1,1,1,1,1,1,0,0,0},
                    {1,1,1,1,1,1,1,0,0,0}};
+
+              
+    int K[10][10]={{0,0,0,0,0,0,0,0,0,0},
+                   {0,0,0,0,0,0,0,0,0,0},
+                   {0,0,0,0,0,0,0,0,0,0},
+                   {0,0,0,0,0,0,0,0,0,0},
+                   {0,0,0,0,0,0,0,0,0,0},
+                   {0,0,0,0,0,0,0,0,0,0},
+                   {0,0,0,0,0,0,0,0,0,0},
+                   {0,0,0,0,0,0,0,0,0,0},
+                   {0,0,0,0,0,0,0,0,0,0},
+                   {0,0,0,0,0,0,0,0,0,0}};
+
+          
+    int L[10][10]={{0,0,0,0,0,0,0,0,0,0},
+                   {0,0,0,0,0,0,0,0,0,0},
+                   {0,0,0,0,0,0,0,0,0,0},
+                   {0,0,0,0,0,0,0,0,0,0},
+                   {0,0,0,0,0,0,0,0,0,0},
+                   {0,0,0,0,0,0,0,0,0,0},
+                   {0,0,0,0,0,0,0,0,0,0},
+                   {0,0,0,0,0,0,0,0,0,0},
+                   {0,0,0,0,0,0,0,0,0,0},
+                   {0,0,0,0,0,0,0,0,0,0}};
+          
+    int M[10][10]={{0,0,0,0,0,0,0,0,0,0},
+                   {0,0,0,0,0,0,0,0,0,0},
+                   {0,0,0,0,0,0,0,0,0,0},
+                   {0,0,0,0,0,0,0,0,0,0},
+                   {0,0,0,0,0,0,0,0,0,0},
+                   {0,0,0,0,0,0,0,0,0,0},
+                   {0,0,0,0,0,0,0,0,0,0},
+                   {0,0,0,0,0,0,0,0,0,0},
+                   {0,0,0,0,0,0,0,0,0,0},
+                   {0,0,0,0,0,0,0,0,0,0}};
+          
+    int N[10][10]={{0,0,0,0,0,0,0,0,0,0},
+                   {0,0,0,0,0,0,0,0,0,0},
+                   {0,0,0,0,0,0,0,0,0,0},
+                   {0,0,0,0,0,0,0,0,0,0},
+                   {0,0,0,0,0,0,0,0,0,0},
+                   {0,0,0,0,0,0,0,0,0,0},
+                   {0,0,0,0,0,0,0,0,0,0},
+                   {0,0,0,0,0,0,0,0,0,0},
+                   {0,0,0,0,0,0,0,0,0,0},
+                   {0,0,0,0,0,0,0,0,0,0}};
+          
+    int O[10][10]={{0,0,0,0,0,0,0,0,0,0},
+                   {0,0,0,0,0,0,0,0,0,0},
+                   {0,0,0,0,0,0,0,0,0,0},
+                   {0,0,0,0,0,0,0,0,0,0},
+                   {0,0,0,0,0,0,0,0,0,0},
+                   {0,0,0,0,0,0,0,0,0,0},
+                   {0,0,0,0,0,0,0,0,0,0},
+                   {0,0,0,0,0,0,0,0,0,0},
+                   {0,0,0,0,0,0,0,0,0,0},
+                   {0,0,0,0,0,0,0,0,0,0}};
+
+    
+    int P[10][10]={{1,1,1,1,1,1,1,0,0,0},
+                   {1,1,1,1,1,1,1,1,0,0},
+                   {1,1,0,0,0,0,0,1,1,0},
+                   {1,1,0,0,0,0,0,1,1,0},
+                   {1,1,0,0,0,0,0,1,1,0},
+                   {1,1,1,1,1,1,1,1,0,0},
+                   {1,1,1,1,1,1,1,0,0,0},
+                   {1,1,0,0,0,0,0,0,0,0},
+                   {1,1,0,0,0,0,0,0,0,0},
+                   {1,1,0,0,0,0,0,0,0,0}};
+
+          
+    int Q[10][10]={{0,0,0,0,0,0,0,0,0,0},
+                   {0,0,0,0,0,0,0,0,0,0},
+                   {0,0,0,0,0,0,0,0,0,0},
+                   {0,0,0,0,0,0,0,0,0,0},
+                   {0,0,0,0,0,0,0,0,0,0},
+                   {0,0,0,0,0,0,0,0,0,0},
+                   {0,0,0,0,0,0,0,0,0,0},
+                   {0,0,0,0,0,0,0,0,0,0},
+                   {0,0,0,0,0,0,0,0,0,0},
+                   {0,0,0,0,0,0,0,0,0,0}};
+          
+    int R[10][10]={{0,0,0,0,0,0,0,0,0,0},
+                   {0,0,0,0,0,0,0,0,0,0},
+                   {0,0,0,0,0,0,0,0,0,0},
+                   {0,0,0,0,0,0,0,0,0,0},
+                   {0,0,0,0,0,0,0,0,0,0},
+                   {0,0,0,0,0,0,0,0,0,0},
+                   {0,0,0,0,0,0,0,0,0,0},
+                   {0,0,0,0,0,0,0,0,0,0},
+                   {0,0,0,0,0,0,0,0,0,0},
+                   {0,0,0,0,0,0,0,0,0,0}};
+
+          
+    int S[10][10]={{0,0,0,0,0,0,0,0,0,0},
+                   {0,0,0,0,0,0,0,0,0,0},
+                   {0,0,0,0,0,0,0,0,0,0},
+                   {0,0,0,0,0,0,0,0,0,0},
+                   {0,0,0,0,0,0,0,0,0,0},
+                   {0,0,0,0,0,0,0,0,0,0},
+                   {0,0,0,0,0,0,0,0,0,0},
+                   {0,0,0,0,0,0,0,0,0,0},
+                   {0,0,0,0,0,0,0,0,0,0},
+                   {0,0,0,0,0,0,0,0,0,0}};
+          
+    int T[10][10]={{0,0,0,0,0,0,0,0,0,0},
+                   {0,0,0,0,0,0,0,0,0,0},
+                   {0,0,0,0,0,0,0,0,0,0},
+                   {0,0,0,0,0,0,0,0,0,0},
+                   {0,0,0,0,0,0,0,0,0,0},
+                   {0,0,0,0,0,0,0,0,0,0},
+                   {0,0,0,0,0,0,0,0,0,0},
+                   {0,0,0,0,0,0,0,0,0,0},
+                   {0,0,0,0,0,0,0,0,0,0},
+                   {0,0,0,0,0,0,0,0,0,0}};
+          
+    int U[10][10]={{0,0,0,0,0,0,0,0,0,0},
+                   {0,0,0,0,0,0,0,0,0,0},
+                   {0,0,0,0,0,0,0,0,0,0},
+                   {0,0,0,0,0,0,0,0,0,0},
+                   {0,0,0,0,0,0,0,0,0,0},
+                   {0,0,0,0,0,0,0,0,0,0},
+                   {0,0,0,0,0,0,0,0,0,0},
+                   {0,0,0,0,0,0,0,0,0,0},
+                   {0,0,0,0,0,0,0,0,0,0},
+                   {0,0,0,0,0,0,0,0,0,0}};
+          
+    int V[10][10]={{0,0,0,0,0,0,0,0,0,0},
+                   {0,0,0,0,0,0,0,0,0,0},
+                   {0,0,0,0,0,0,0,0,0,0},
+                   {0,0,0,0,0,0,0,0,0,0},
+                   {0,0,0,0,0,0,0,0,0,0},
+                   {0,0,0,0,0,0,0,0,0,0},
+                   {0,0,0,0,0,0,0,0,0,0},
+                   {0,0,0,0,0,0,0,0,0,0},
+                   {0,0,0,0,0,0,0,0,0,0},
+                   {0,0,0,0,0,0,0,0,0,0}};
+          
+    int W[10][10]={{0,0,0,0,0,0,0,0,0,0},
+                   {0,0,0,0,0,0,0,0,0,0},
+                   {0,0,0,0,0,0,0,0,0,0},
+                   {0,0,0,0,0,0,0,0,0,0},
+                   {0,0,0,0,0,0,0,0,0,0},
+                   {0,0,0,0,0,0,0,0,0,0},
+                   {0,0,0,0,0,0,0,0,0,0},
+                   {0,0,0,0,0,0,0,0,0,0},
+                   {0,0,0,0,0,0,0,0,0,0},
+                   {0,0,0,0,0,0,0,0,0,0}};
+
+    int X[10][10]={{0,0,0,0,0,0,0,0,0,0},
+                   {0,0,0,0,0,0,0,0,0,0},
+                   {0,0,0,0,0,0,0,0,0,0},
+                   {0,0,0,0,0,0,0,0,0,0},
+                   {0,0,0,0,0,0,0,0,0,0},
+                   {0,0,0,0,0,0,0,0,0,0},
+                   {0,0,0,0,0,0,0,0,0,0},
+                   {0,0,0,0,0,0,0,0,0,0},
+                   {0,0,0,0,0,0,0,0,0,0},
+                   {0,0,0,0,0,0,0,0,0,0}};
+
+    int Y[10][10]={{0,0,0,0,0,0,0,0,0,0},
+                   {0,0,0,0,0,0,0,0,0,0},
+                   {0,0,0,0,0,0,0,0,0,0},
+                   {0,0,0,0,0,0,0,0,0,0},
+                   {0,0,0,0,0,0,0,0,0,0},
+                   {0,0,0,0,0,0,0,0,0,0},
+                   {0,0,0,0,0,0,0,0,0,0},
+                   {0,0,0,0,0,0,0,0,0,0},
+                   {0,0,0,0,0,0,0,0,0,0},
+                   {0,0,0,0,0,0,0,0,0,0}};
+          
+    int Z[10][10]={{0,0,0,0,0,0,0,0,0,0},
+                   {0,0,0,0,0,0,0,0,0,0},
+                   {0,0,0,0,0,0,0,0,0,0},
+                   {0,0,0,0,0,0,0,0,0,0},
+                   {0,0,0,0,0,0,0,0,0,0},
+                   {0,0,0,0,0,0,0,0,0,0},
+                   {0,0,0,0,0,0,0,0,0,0},
+                   {0,0,0,0,0,0,0,0,0,0},
+                   {0,0,0,0,0,0,0,0,0,0},
+                   {0,0,0,0,0,0,0,0,0,0}};
+                   
+
 
     // Open the file for reading and writing
     fbfd = open("/dev/fb0", O_RDWR);
@@ -214,22 +398,124 @@ int main()
     // Figure out where in memory to put the pixel
     // printf("%d\n",C[0][0]);
     int i = 1079;
-    int spaceOneLine = 100;
-    int numberLine = 6;
-    int spaceOfLine = 50;
-    while(i+150 > 0) {
-        writeCharacter(fbp, vinfo, finfo, i, 100, 0, C);
-        writeCharacter(fbp, vinfo, finfo, i, 200, 0, H);
-        writeCharacter(fbp, vinfo, finfo, i, 300, 0, E);
-        writeCharacter(fbp, vinfo, finfo, i, 400, 0, E);
-        writeCharacter(fbp, vinfo, finfo, i, 500, 0, P);
-        writeCharacter(fbp, vinfo, finfo, i, 600, 0, E);
-        writeCharacter(fbp, vinfo, finfo, i, 700, 0, E);
-        writeCharacter(fbp, vinfo, finfo, i, 800, 0, D);
-        writeCharacter(fbp, vinfo, finfo, i + spaceOfLine + spaceOneLine, 100,0,F);
-        writeCharacter(fbp, vinfo, finfo, i + spaceOfLine + spaceOneLine, 200,0,G);
-        writeCharacter(fbp, vinfo, finfo, i + spaceOfLine + spaceOneLine, 300,0,I);
-        writeCharacter(fbp, vinfo, finfo, i + spaceOfLine + spaceOneLine, 400,0,J);
+    int numberLine = 7;
+    int spaceOfLine = 150;
+    char  *listNama[] = {"cheepeed",
+                         "jason",
+                         "ensof",
+                         "steven",
+                         "ricky",
+                         "ejo",
+                         "cis"
+                        };
+
+    while(i+(numberLine*spaceOfLine) > 0) {
+        
+        int len = 0;
+        int titikAwal = 0 ;
+        for(int iterator = 0; iterator < 7; iterator++){
+            len = strlen(listNama[iterator]);
+            // printf("%d %s",len, listNama[iterator]);
+            titikAwal = 100;
+            if (iterator == 0) {
+                titikAwal += 500;
+            }
+            for (int it = 0;it < len ; it++) {
+                if (listNama[iterator][it] == 'a') {
+                    writeCharacter(fbp, vinfo, finfo, i+(iterator*spaceOfLine), titikAwal, 0, A);
+                }
+                if (listNama[iterator][it] == 'b') {
+                    writeCharacter(fbp, vinfo, finfo, i+(iterator*spaceOfLine), titikAwal, 0, B);
+                }
+                if (listNama[iterator][it] == 'c') {
+                    writeCharacter(fbp, vinfo, finfo, i+(iterator*spaceOfLine), titikAwal, 0, C);
+                }
+                if (listNama[iterator][it] == 'e') {
+                    writeCharacter(fbp, vinfo, finfo, i+(iterator*spaceOfLine), titikAwal, 0, E);
+                }
+                if (listNama[iterator][it] == 'f') {
+                    writeCharacter(fbp, vinfo, finfo, i+(iterator*spaceOfLine), titikAwal, 0, F);
+                }
+                if (listNama[iterator][it] == 'g') {
+                    writeCharacter(fbp, vinfo, finfo, i+(iterator*spaceOfLine), titikAwal, 0, G);
+                }
+                if (listNama[iterator][it] == 'h') {
+                    writeCharacter(fbp, vinfo, finfo, i+(iterator*spaceOfLine), titikAwal, 0, H);
+                }
+                if (listNama[iterator][it] == 'i') {
+                    writeCharacter(fbp, vinfo, finfo, i+(iterator*spaceOfLine), titikAwal, 0, I);
+                }
+                if (listNama[iterator][it] == 'j') {
+                    writeCharacter(fbp, vinfo, finfo, i+(iterator*spaceOfLine), titikAwal, 0, J);
+                }
+                if (listNama[iterator][it] == 'k') {
+                    writeCharacter(fbp, vinfo, finfo, i+(iterator*spaceOfLine), titikAwal, 0, K);
+                }
+                if (listNama[iterator][it] == 'l') {
+                    writeCharacter(fbp, vinfo, finfo, i+(iterator*spaceOfLine), titikAwal, 0, L);
+                }
+                if (listNama[iterator][it] == 'm') {
+                    writeCharacter(fbp, vinfo, finfo, i+(iterator*spaceOfLine), titikAwal, 0, M);
+                }
+                if (listNama[iterator][it] == 'n') {
+                    writeCharacter(fbp, vinfo, finfo, i+(iterator*spaceOfLine), titikAwal, 0, N);
+                }
+                if (listNama[iterator][it] == 'o') {
+                    writeCharacter(fbp, vinfo, finfo, i+(iterator*spaceOfLine), titikAwal, 0, O);
+                }
+                if (listNama[iterator][it] == 'p') {
+                    writeCharacter(fbp, vinfo, finfo, i+(iterator*spaceOfLine), titikAwal, 0, P);
+                }
+                if (listNama[iterator][it] == 'q') {
+                    writeCharacter(fbp, vinfo, finfo, i+(iterator*spaceOfLine), titikAwal, 0, Q);
+                }
+                if (listNama[iterator][it] == 'r') {
+                    writeCharacter(fbp, vinfo, finfo, i+(iterator*spaceOfLine), titikAwal, 0, R);
+                }
+                if (listNama[iterator][it] == 's') {
+                    writeCharacter(fbp, vinfo, finfo, i+(iterator*spaceOfLine), titikAwal, 0, S);
+                }
+                if (listNama[iterator][it] == 't') {
+                    writeCharacter(fbp, vinfo, finfo, i+(iterator*spaceOfLine), titikAwal, 0, T);
+                }
+                if (listNama[iterator][it] == 'u') {
+                    writeCharacter(fbp, vinfo, finfo, i+(iterator*spaceOfLine), titikAwal, 0, U);
+                }
+                if (listNama[iterator][it] == 'v') {
+                    writeCharacter(fbp, vinfo, finfo, i+(iterator*spaceOfLine), titikAwal, 0, V);
+                }
+                if (listNama[iterator][it] == 'w') {
+                    writeCharacter(fbp, vinfo, finfo, i+(iterator*spaceOfLine), titikAwal, 0, W);
+                }
+                if (listNama[iterator][it] == 'x') {
+                    writeCharacter(fbp, vinfo, finfo, i+(iterator*spaceOfLine), titikAwal, 0, X);
+                }
+                if (listNama[iterator][it] == 'y') {
+                    writeCharacter(fbp, vinfo, finfo, i+(iterator*spaceOfLine), titikAwal, 0, Y);
+                }
+                if (listNama[iterator][it] == 'z') {
+                    writeCharacter(fbp, vinfo, finfo, i+(iterator*spaceOfLine), titikAwal, 0, Z);
+                }
+                if (listNama[iterator][it] == ' ') {
+                    titikAwal+=100;
+                }
+
+                titikAwal+=100;       
+            }
+        
+        }
+        // writeCharacter(fbp, vinfo, finfo, i, 100, 0, C);
+        // writeCharacter(fbp, vinfo, finfo, i, 200, 0, H);
+        // writeCharacter(fbp, vinfo, finfo, i, 300, 0, E);
+        // writeCharacter(fbp, vinfo, finfo, i, 400, 0, E);
+        // writeCharacter(fbp, vinfo, finfo, i, 500, 0, P);
+        // writeCharacter(fbp, vinfo, finfo, i, 600, 0, E);
+        // writeCharacter(fbp, vinfo, finfo, i, 700, 0, E);
+        // writeCharacter(fbp, vinfo, finfo, i, 800, 0, D);
+        // writeCharacter(fbp, vinfo, finfo, i + spaceOfLine , 100,0,F);
+        // writeCharacter(fbp, vinfo, finfo, i + spaceOfLine , 200,0,G);
+        // writeCharacter(fbp, vinfo, finfo, i + spaceOfLine , 300,0,I);
+        // writeCharacter(fbp, vinfo, finfo, i + spaceOfLine , 400,0,J);
 
 
         usleep(90000);
@@ -238,7 +524,7 @@ int main()
             y = 1;
         }
         while((y<1080)) {
-            x = 100;
+            x = 0;
             while(x < 1900) {
 
                 location = (x+vinfo.xoffset) * (vinfo.bits_per_pixel/8) +
