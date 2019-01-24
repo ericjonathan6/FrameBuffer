40aab2b5499ce96aaba6c2df814b9da828e1869b
ebarhami <ensof.intern@stoqo.com>
2019-01-24 10:31:03 +0700

    add general bressenham

--- line.c
+++ line.c
@@ -22,30 +22,53 @@ struct Point {
    int x, y; 
 };  
   
-// function for line generation 
 struct Point* bresenham(struct Point A, struct Point B){ 
-   int m_new = 2 * (B.y - A.y);  //multiple by 2 to avoid comparison with 0.5
-   // prove : https://csustan.csustan.edu/~tom/Lecture-Notes/Graphics/Bresenham-Line/Bresenham-Line.pdf
-   int slope_error_new = m_new - (B.x - A.x);
-   struct Point result_points[5000], new_point;
-   int iterator = 0;
-   for (int x = A.x, y = A.y; x <= B.x; x++){ 
-      
-      new_point.x = x;
-      new_point.y = y;
+   int dx = B.x - A.x;
+   int dy = B.y - A.y;
+   int x0 = A.x, y0 = A.y;
+   int x1 = B.x, y1 = B.y;
+   int stepx,stepy;
+   struct Point result_points[5000];
+   result_points[0].x = x0;
+   result_points[0].y = y0;
+   int iterator = 1;
+   if (dy < 0) { dy = -dy;  stepy = -1; } else { stepy = 1; }
+   if (dx < 0) { dx = -dx;  stepx = -1; } else { stepx = 1; }
+   dy <<= 1;                        
+   dx <<= 1;                        
 
-      result_points[iterator] = new_point;
-      slope_error_new += m_new;  
-      if (slope_error_new >= 0){ 
-         y++; 
-         slope_error_new  -= 2 * (B.x - A.x); 
-      } 
-      iterator++;
-   } 
-   // for(int i=0;i<iterator;i++){
-   //    printf("%d, %d\n", result_points[i].x,result_points[i].y);
-   // }
-   return result_points;
+   if (dx > dy) {
+      int fraction = dy - (dx >> 1);
+      while (x0 != x1) {
+         x0 += stepx;
+         if (fraction >= 0) {
+            y0 += stepy;
+            fraction -= dx;
+         }
+         fraction += dy;
+         result_points[iterator].x = x0;
+         result_points[iterator].y = y0;
+         iterator++;
+      }
+   } else {
+      int fraction = dx - (dy >> 1);
+      while (y0 != y1) {
+         y0 += stepy;
+         
+         if (fraction >= 0) {
+            x0 += stepx;
+            fraction -= dy;
+         }
+         
+         fraction += dx;
+         result_points[iterator].x = x0;
+         result_points[iterator].y = y0;
+         iterator++;
+      }
+   }
+   for(int i=0;i<iterator;i++){
+      printf("%d, %d\n", result_points[i].x,result_points[i].y);
+   }
 }
 
 int main()
