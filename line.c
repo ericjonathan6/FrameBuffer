07979dec2b6ce48ae5de5d7e0bd3f638b6c92d57
Steven Sukma <steven.sukma.limanus@gmail.com>
2019-01-27 19:06:29 +0700

    [FIX] draw shots

--- line.c
+++ line.c
@@ -250,7 +250,7 @@ int main()
     A.y = 870;
 
     B.x = 960;
-    B.y = 251;
+    B.y = 845;
     int xoffset = 20;
 
     x = 0; y = 0;       // Where we are going to put the pixel
@@ -274,94 +274,33 @@ int main()
 
 
     drawCannon(vinfo, finfo, fbp);
-    while(1) {        
-        int dx = B.x - A.x;
-        int dy = B.y - A.y;
-        int x0 = A.x, y0 = A.y;
-        int x1 = B.x, y1 = B.y;
-        int stepx,stepy;
-        int iterator = 0;
-        long int location = 0;
-        if (dy < 0) { dy = -dy;  stepy = -1; } else { stepy = 1; }
-        if (dx < 0) { dx = -dx;  stepx = -1; } else { stepx = 1; }
-        dy <<= 1;                        
-        dx <<= 1;                        
-        
-        if (dx > dy) {
-            int fraction = dy - (dx >> 1);
-            while (x0 != x1) {
-                x0 += stepx;
-                if (fraction >= 0) {
-                y0 += stepy;
-                fraction -= dx;
-                }
-                fraction += dy;
-                location = (x0+vinfo.xoffset) * (vinfo.bits_per_pixel/8) +
-                            (y0+vinfo.yoffset) * finfo.line_length;
-
-                if (vinfo.bits_per_pixel == 32) {
-                    *(fbp + location) = 255;        // Some blue
-                    *(fbp + location + 1) = 255;     // A little green
-                    *(fbp + location + 2) = 255;    // A lot of red
-                    *(fbp + location + 3) = 0;      // No transparency
-                }
-                iterator++;
-                drawShip(xoffset, vinfo, finfo, fbp, direction);
-                if (iterator == 25) {
-                    usleep(50000);
-                    clearShip(vinfo, finfo, fbp);
-                    clearShot(vinfo, finfo, fbp);
-                    iterator = 0;
-                    if(direction) {
-                        xoffset += 5;
-                    } else {
-                        xoffset -=5;
-                    }
-                }
-            }
+    while(1) {                            
+        drawShip(xoffset, vinfo, finfo, fbp, direction);
+        drawLines(A, B, vinfo, finfo, fbp);
+        usleep(50000);
+        clearShip(vinfo, finfo, fbp);
+        clearShot(vinfo, finfo, fbp);
+        if(direction) {
+            xoffset += 5;
         } else {
-            int fraction = dx - (dy >> 1);
-            while (y0 != y1) {
-                y0 += stepy;
-                
-                if (fraction >= 0) {
-                x0 += stepx;
-                fraction -= dy;
-                }
-                
-                fraction += dx;
-                location = (x0+vinfo.xoffset) * (vinfo.bits_per_pixel/8) +
-                            (y0+vinfo.yoffset) * finfo.line_length;
-
-                if (vinfo.bits_per_pixel == 32) {
-                    *(fbp + location) = 255;        // Some blue
-                    *(fbp + location + 1) = 255;     // A little green
-                    *(fbp + location + 2) = 255;    // A lot of red
-                    *(fbp + location + 3) = 0;      // No transparency
-                }
-                iterator++;
-                drawShip(xoffset, vinfo, finfo, fbp, direction);
-                if (iterator == 25) {
-                    usleep(50000);
-                    clearShip(vinfo, finfo, fbp);
-                    clearShot(vinfo, finfo, fbp);
-                    iterator = 0;
-                    if(direction) {
-                        xoffset += 5;
-                    } else {
-                        xoffset -=5;
-                    }
-
-                    if(xoffset < 20) {
-                        direction = 1;
-                    }
-
-                    if(xoffset > 1770) {
-                        direction = 0;
-                    }
-                }
-            }
+            xoffset -=5;
         }
+
+        if(xoffset < 20) {
+            direction = 1;
+        }
+
+        if(xoffset > 1770) {
+            direction = 0;
+        }
+
+        A.y = B.y;
+        B.y -= 25;
+
+        if(B.y < 251) {
+            B.y = 251;
+        }
+                
         
     }
 
