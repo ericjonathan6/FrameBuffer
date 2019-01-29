8a5181d881b0db5be60c9abac98a6cc16a8ccee4
Eric Jonathan <eric-i@sellfazz.com>
2019-01-29 17:19:26 +0700

    change const

--- line.c
+++ line.c
@@ -19,6 +19,10 @@ http://cep.xor.aps.anl.gov/software/qt4-x11-4.2.2/qtopiacore-testingframebuffer.
 #include <sys/mman.h>
 #include <sys/ioctl.h>
 
+const int WIDTH = 800;
+const int HEIGHT = 600;
+const int WIDTH_MARGIN = 20;
+
 struct Point { 
    int x, y; 
 };  
@@ -33,9 +37,6 @@ int kbhit(void)
         return 0;
     }
 }
-int width = 800;
-int height = 600;
-int width_margin = 20;
 
 struct Point rotate_point(int cx,int cy,float angle,struct Point p)
 {
@@ -59,17 +60,20 @@ struct Point rotate_point(int cx,int cy,float angle,struct Point p)
 struct Point find_center(struct Point A, struct Point B, struct Point C, struct Point D)
 {
   struct Point E;
-  E.x = (int) (A.x+B.x)/2;
-  E.y = (int) (A.y+D.y)/2;
+
+  E.x = (int) (A.x + B.x) / 2;
+  E.y = (int) (A.y + D.y) / 2;
+  
   return E;
 }
 
 void clearShot(struct fb_var_screeninfo vinfo, struct fb_fix_screeninfo finfo, char *fbp) {
-    int y = 251*height/1080;
+    int y = 251 * WIDTH / 1080;
     long int location = 0;
-    while((y<949*height/1080)) {
+
+    while((y < 949 * HEIGHT / 1080)) {
         int x = 0;
-        while(x < width - width_margin*width/1080) {
+        while(x < WIDTH - WIDTH_MARGIN * WIDTH / 1080) {
             location = (x+vinfo.xoffset) * (vinfo.bits_per_pixel/8) +
                     (y+vinfo.yoffset) * finfo.line_length;
 
@@ -93,8 +97,20 @@ void drawLines(struct Point A, struct Point B, struct fb_var_screeninfo vinfo, s
     int stepx,stepy;
     int iterator = 0;
     long int location = 0;
-    if (dy < 0) { dy = -dy;  stepy = -1; } else { stepy = 1; }
-    if (dx < 0) { dx = -dx;  stepx = -1; } else { stepx = 1; }
+
+    if (dy < 0) {
+        dy = -dy;  stepy = -1;
+    } else {
+        stepy = 1;
+    }
+    
+    if (dx < 0) {
+        dx = -dx;
+        stepx = -1;
+    } else {
+        stepx = 1;
+    }
+
     dy <<= 1;                        
     dx <<= 1;                        
     
@@ -147,69 +163,69 @@ void destroyShip(int xoffset, struct fb_var_screeninfo vinfo, struct fb_fix_scre
 
     if (direction == 1) {
         A.x = xoffset;
-        B.x = xoffset + 30*width/1920;
-        C.x = xoffset + 30*width/1920;
-        D.x = xoffset + 60*width/1920;
-        E.x = xoffset + 70*width/1920;    
-        F.x = xoffset + 110*width/1920;
-        G.x = xoffset + 90*width/1920;
-        H.x = xoffset + 10*width/1920;
-        X.x = xoffset + 30*width/1920;
-        Z.x = xoffset + 70*width/1920;
+        B.x = xoffset + 30 * WIDTH / 1920;
+        C.x = xoffset + 30 * WIDTH / 1920;
+        D.x = xoffset + 60 * WIDTH / 1920;
+        E.x = xoffset + 70 * WIDTH / 1920;    
+        F.x = xoffset + 110 * WIDTH / 1920;
+        G.x = xoffset + 90 * WIDTH / 1920;
+        H.x = xoffset + 10 * WIDTH / 1920;
+        X.x = xoffset + 30 * WIDTH / 1920;
+        Z.x = xoffset + 70 * WIDTH / 1920;
 
-        f1.x = xoffset + 15*width/1920;
-        f2.x = xoffset + 15*width/1920;
-        f3.x = xoffset + 15*width/1920;
-        f4.x = xoffset + 15*width/1920;
-        f5.x = xoffset - 15*width/1920;
-        f6.x = xoffset - 15*width/1920;
-        f7.x = xoffset - 15*width/1920;
+        f1.x = xoffset + 15 * WIDTH / 1920;
+        f2.x = xoffset + 15 * WIDTH / 1920;
+        f3.x = xoffset + 15 * WIDTH / 1920;
+        f4.x = xoffset + 15 * WIDTH / 1920;
+        f5.x = xoffset - 15 * WIDTH / 1920;
+        f6.x = xoffset - 15 * WIDTH / 1920;
+        f7.x = xoffset - 15 * WIDTH / 1920;
 
     } else {
         A.x = xoffset;        
-        B.x = xoffset + 40*width/1920;
-        C.x = xoffset + 50*width/1920;
-        D.x = xoffset + 80*width/1920;
-        E.x = xoffset + 80*width/1920;
-        F.x = xoffset + 110*width/1920;
-        G.x = xoffset + 100*width/1920;
-        H.x = xoffset + 20*width/1920;
-        X.x = xoffset + 40*width/1920;
-        Z.x = xoffset + 80*width/1920;
+        B.x = xoffset + 40 * WIDTH / 1920;
+        C.x = xoffset + 50 * WIDTH / 1920;
+        D.x = xoffset + 80 * WIDTH / 1920;
+        E.x = xoffset + 80 * WIDTH / 1920;
+        F.x = xoffset + 110 * WIDTH / 1920;
+        G.x = xoffset + 100 * WIDTH / 1920;
+        H.x = xoffset + 20 * WIDTH / 1920;
+        X.x = xoffset + 40 * WIDTH / 1920;
+        Z.x = xoffset + 80 * WIDTH / 1920;
     
-        f1.x = xoffset + 95*width/1920;
-        f2.x = xoffset + 95*width/1920;
-        f3.x = xoffset + 95*width/1920;
-        f4.x = xoffset + 95*width/1920;
-        f5.x = xoffset + 125*width/1920;
-        f6.x = xoffset + 125*width/1920;
-        f7.x = xoffset + 125*width/1920;
+        f1.x = xoffset + 95 * WIDTH / 1920;
+        f2.x = xoffset + 95 * WIDTH / 1920;
+        f3.x = xoffset + 95 * WIDTH / 1920;
+        f4.x = xoffset + 95 * WIDTH / 1920;
+        f5.x = xoffset + 125 * WIDTH / 1920;
+        f6.x = xoffset + 125 * WIDTH / 1920;
+        f7.x = xoffset + 125 * WIDTH / 1920;
     }
-    A.y = 200*height/1080;
-    B.y = 200*height/1080;
-    C.y = 165*height/1080;
-    D.y = 165*height/1080;
-    E.y = 200*height/1080;
-    F.y = 200*height/1080;
-    G.y = 250*height/1080;
-    H.y = 250*height/1080;
-    X.y = 220*height/1080;
-    Z.y = 220*height/1080;
+    A.y = 200 * HEIGHT / 1080;
+    B.y = 200 * HEIGHT / 1080;
+    C.y = 165 * HEIGHT / 1080;
+    D.y = 165 * HEIGHT / 1080;
+    E.y = 200 * HEIGHT / 1080;
+    F.y = 200 * HEIGHT / 1080;
+    G.y = 250 * HEIGHT / 1080;
+    H.y = 250 * HEIGHT / 1080;
+    X.y = 220 * HEIGHT / 1080;
+    Z.y = 220 * HEIGHT / 1080;
 
-    f1.y = 200*height/1080;
-    f2.y = 180*height/1080;
-    f3.y = 165*height/1080;
-    f4.y = 150*height/1080;
-    f5.y = 150*height/1080;
-    f6.y = 165*height/1080;
-    f7.y = 180*height/1080;
+    f1.y = 200 * HEIGHT / 1080;
+    f2.y = 180 * HEIGHT / 1080;
+    f3.y = 165 * HEIGHT / 1080;
+    f4.y = 150 * HEIGHT / 1080;
+    f5.y = 150 * HEIGHT / 1080;
+    f6.y = 165 * HEIGHT / 1080;
+    f7.y = 180 * HEIGHT / 1080;
 
     struct Point awal, akhir;
-    awal.x = 960*width/1920;
-    awal.y = 870*height/1080;
+    awal.x = 960 * WIDTH / 1920;
+    awal.y = 870 * HEIGHT / 1080;
 
-    akhir.x = 960*width/1920;
-    akhir.y = 251*height/1080;
+    akhir.x = 960 * WIDTH / 1920;
+    akhir.y = 251 * HEIGHT / 1080;
 
     clearShip(vinfo, finfo, fbp);
 
@@ -293,63 +309,63 @@ void drawShip(int xoffset, struct fb_var_screeninfo vinfo, struct fb_fix_screeni
 
     if (direction == 1) {
         A.x = xoffset;
-        B.x = xoffset + 30*width/1920;
-        C.x = xoffset + 30*width/1920;
-        D.x = xoffset + 60*width/1920;
-        E.x = xoffset + 70*width/1920;    
-        F.x = xoffset + 110*width/1920;
-        G.x = xoffset + 90*width/1920;
-        H.x = xoffset + 10*width/1920;
+        B.x = xoffset + 30 * WIDTH / 1920;
+        C.x = xoffset + 30 * WIDTH / 1920;
+        D.x = xoffset + 60 * WIDTH / 1920;
+        E.x = xoffset + 70 * WIDTH / 1920;    
+        F.x = xoffset + 110 * WIDTH / 1920;
+        G.x = xoffset + 90 * WIDTH / 1920;
+        H.x = xoffset + 10 * WIDTH / 1920;
 
-        f1.x = xoffset + 15*width/1920;
-        f2.x = xoffset + 15*width/1920;
-        f3.x = xoffset + 15*width/1920;
-        f4.x = xoffset + 15*width/1920;
-        f5.x = xoffset - 15*width/1920;
-        f6.x = xoffset - 15*width/1920;
-        f7.x = xoffset - 15*width/1920;
+        f1.x = xoffset + 15 * WIDTH / 1920;
+        f2.x = xoffset + 15 * WIDTH / 1920;
+        f3.x = xoffset + 15 * WIDTH / 1920;
+        f4.x = xoffset + 15 * WIDTH / 1920;
+        f5.x = xoffset - 15 * WIDTH / 1920;
+        f6.x = xoffset - 15 * WIDTH / 1920;
+        f7.x = xoffset - 15 * WIDTH / 1920;
 
     } else {
         A.x = xoffset;        
-        B.x = xoffset + 40*width/1920;
-        C.x = xoffset + 50*width/1920;
-        D.x = xoffset + 80*width/1920;
-        E.x = xoffset + 80*width/1920;
-        F.x = xoffset + 110*width/1920;
-        G.x = xoffset + 100*width/1920;
-        H.x = xoffset + 20*width/1920;
+        B.x = xoffset + 40 * WIDTH / 1920;
+        C.x = xoffset + 50 * WIDTH / 1920;
+        D.x = xoffset + 80 * WIDTH / 1920;
+        E.x = xoffset + 80 * WIDTH / 1920;
+        F.x = xoffset + 110 * WIDTH / 1920;
+        G.x = xoffset + 100 * WIDTH / 1920;
+        H.x = xoffset + 20 * WIDTH / 1920;
     
-        f1.x = xoffset + 95*width/1920;
-        f2.x = xoffset + 95*width/1920;
-        f3.x = xoffset + 95*width/1920;
-        f4.x = xoffset + 95*width/1920;
-        f5.x = xoffset + 125*width/1920;
-        f6.x = xoffset + 125*width/1920;
-        f7.x = xoffset + 125*width/1920;
+        f1.x = xoffset + 95 * WIDTH / 1920;
+        f2.x = xoffset + 95 * WIDTH / 1920;
+        f3.x = xoffset + 95 * WIDTH / 1920;
+        f4.x = xoffset + 95 * WIDTH / 1920;
+        f5.x = xoffset + 125 * WIDTH / 1920;
+        f6.x = xoffset + 125 * WIDTH / 1920;
+        f7.x = xoffset + 125 * WIDTH / 1920;
     }
-    A.y = 200*height/1080;
-    B.y = 200*height/1080;
-    C.y = 165*height/1080;
-    D.y = 165*height/1080;
-    E.y = 200*height/1080;
-    F.y = 200*height/1080;
-    G.y = 250*height/1080;
-    H.y = 250*height/1080;
+    A.y = 200 * HEIGHT / 1080;
+    B.y = 200 * HEIGHT / 1080;
+    C.y = 165 * HEIGHT / 1080;
+    D.y = 165 * HEIGHT / 1080;
+    E.y = 200 * HEIGHT / 1080;
+    F.y = 200 * HEIGHT / 1080;
+    G.y = 250 * HEIGHT / 1080;
+    H.y = 250 * HEIGHT / 1080;
 
-    f1.y = 200*height/1080;
-    f2.y = 180*height/1080;
-    f3.y = 165*height/1080;
-    f4.y = 150*height/1080;
-    f5.y = 150*height/1080;
-    f6.y = 165*height/1080;
-    f7.y = 180*height/1080;
+    f1.y = 200 * HEIGHT / 1080;
+    f2.y = 180 * HEIGHT / 1080;
+    f3.y = 165 * HEIGHT / 1080;
+    f4.y = 150 * HEIGHT / 1080;
+    f5.y = 150 * HEIGHT / 1080;
+    f6.y = 165 * HEIGHT / 1080;
+    f7.y = 180 * HEIGHT / 1080;
 
     struct Point awal, akhir;
-    awal.x = 960*width/1920;
-    awal.y = 870*height/1080;
+    awal.x = 960 * WIDTH / 1920;
+    awal.y = 870 * HEIGHT / 1080;
 
-    akhir.x = 960*width/1920;
-    akhir.y = 251*height/1080;
+    akhir.x = 960 * WIDTH / 1920;
+    akhir.y = 251 * HEIGHT / 1080;
 
     drawLines(A, F, vinfo, finfo, fbp);
     drawLines(B, C, vinfo, finfo, fbp);
@@ -364,17 +380,14 @@ void drawShip(int xoffset, struct fb_var_screeninfo vinfo, struct fb_fix_screeni
     drawLines(f5, f7, vinfo, finfo, fbp);
     drawLines(f2, f7, vinfo, finfo, fbp);
     drawLines(f3, f6, vinfo, finfo, fbp);
-
-
-
 }
 
 void clearShip(struct fb_var_screeninfo vinfo, struct fb_fix_screeninfo finfo, char *fbp) {
     int y = 0;
     long int location = 0;
-    while((y<251*height/1080)) {
+    while((y<251 * HEIGHT / 1080)) {
         int x = 0;
-        while(x < width) {
+        while(x < WIDTH) {
             location = (x+vinfo.xoffset) * (vinfo.bits_per_pixel/8) +
                     (y+vinfo.yoffset) * finfo.line_length;
 
@@ -392,17 +405,17 @@ void clearShip(struct fb_var_screeninfo vinfo, struct fb_fix_screeninfo finfo, c
 
 void drawBullet(struct Point offset, struct fb_var_screeninfo vinfo, struct fb_fix_screeninfo finfo, char *fbp, int rotate) {
     struct Point A, B, C, D, E;
-    A.x = offset.x - 14*width/1920;
-    A.y = offset.y + 60*height/1080;
+    A.x = offset.x - 14 * WIDTH / 1920;
+    A.y = offset.y + 60 * HEIGHT / 1080;
 
-    B.x = offset.x + 14*width/1920;
-    B.y = offset.y + 60*height/1080;
+    B.x = offset.x + 14 * WIDTH / 1920;
+    B.y = offset.y + 60 * HEIGHT / 1080;
 
-    C.x = offset.x + 14*width/1920;
-    C.y = offset.y + 20*height/1080;
+    C.x = offset.x + 14 * WIDTH / 1920;
+    C.y = offset.y + 20 * HEIGHT / 1080;
 
-    D.x = offset.x - 14*width/1920;
-    D.y = offset.y + 20*height/1080;
+    D.x = offset.x - 14 * WIDTH / 1920;
+    D.y = offset.y + 20 * HEIGHT / 1080;
     
     if (rotate == 1) {
         E = find_center(D, C, B, A);
@@ -434,65 +447,65 @@ void drawBullet(struct Point offset, struct fb_var_screeninfo vinfo, struct fb_f
 
 void drawCannon(struct fb_var_screeninfo vinfo, struct fb_fix_screeninfo finfo, char *fbp) {
     struct Point A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T;
-    A.x = 900*width/1920;
-    A.y = 1050*height/1080;
+    A.x = 900 * WIDTH / 1920;
+    A.y = 1050 * HEIGHT / 1080;
 
-    B.x = 1020*width/1920;
-    B.y = 1050*height/1080;
+    B.x = 1020 * WIDTH / 1920;
+    B.y = 1050 * HEIGHT / 1080;
 
-    C.x = 1020*width/1920;
-    C.y = 1020*height/1080;
+    C.x = 1020 * WIDTH / 1920;
+    C.y = 1020 * HEIGHT / 1080;
 
-    D.x = 985*width/1920;
-    D.y = 995*height/1080;
+    D.x = 985 * WIDTH / 1920;
+    D.y = 995 * HEIGHT / 1080;
 
-    E.x = 985*width/1920;
-    E.y = 965*height/1080;
+    E.x = 985 * WIDTH / 1920;
+    E.y = 965 * HEIGHT / 1080;
 
-    F.x = 990*width/1920;
-    F.y = 965*height/1080;
+    F.x = 990 * WIDTH / 1920;
+    F.y = 965 * HEIGHT / 1080;
 
-    G.x = 990*width/1920;
-    G.y = 960*height/1080;
+    G.x = 990 * WIDTH / 1920;
+    G.y = 960 * HEIGHT / 1080;
 
-    H.x = 930*width/1920;
-    H.y = 960*height/1080;
+    H.x = 930 * WIDTH / 1920;
+    H.y = 960 * HEIGHT / 1080;
 
-    I.x = 930*width/1920;
-    I.y = 965*height/1080;
+    I.x = 930 * WIDTH / 1920;
+    I.y = 965 * HEIGHT / 1080;
 
-    J.x = 935*width/1920;
-    J.y = 965*height/1080;
+    J.x = 935 * WIDTH / 1920;
+    J.y = 965 * HEIGHT / 1080;
 
-    K.x = 935*width/1920;
-    K.y = 995*height/1080;
+    K.x = 935 * WIDTH / 1920;
+    K.y = 995 * HEIGHT / 1080;
 
-    L.x = 900*width/1920;
-    L.y = 1020*height/1080;
+    L.x = 900 * WIDTH / 1920;
+    L.y = 1020 * HEIGHT / 1080;
 
-    M.x = 935*width/1920;
-    M.y = 1050*height/1080;
+    M.x = 935 * WIDTH / 1920;
+    M.y = 1050 * HEIGHT / 1080;
 
-    N.x = 985*width/1920;
-    N.y = 1050*height/1080;
+    N.x = 985 * WIDTH / 1920;
+    N.y = 1050 * HEIGHT / 1080;
 
-    O.x = 935*width/1920;
-    O.y = 1005*height/1080;
+    O.x = 935 * WIDTH / 1920;
+    O.y = 1005 * HEIGHT / 1080;
 
-    P.x = 985*width/1920;
-    P.y = 1005*height/1080;
+    P.x = 985 * WIDTH / 1920;
+    P.y = 1005 * HEIGHT / 1080;
 
-    Q.x = 945*width/1920;
-    Q.y = 960*height/1080;
+    Q.x = 945 * WIDTH / 1920;
+    Q.y = 960 * HEIGHT / 1080;
 
-    R.x = 975*width/1920;
-    R.y = 960*height/1080;
+    R.x = 975 * WIDTH / 1920;
+    R.y = 960 * HEIGHT / 1080;
 
-    S.x = 975*width/1920;
-    S.y = 950*height/1080;
+    S.x = 975 * WIDTH / 1920;
+    S.y = 950 * HEIGHT / 1080;
 
-    T.x = 945*width/1920;
-    T.y = 950*height/1080;
+    T.x = 945 * WIDTH / 1920;
+    T.y = 950 * HEIGHT / 1080;
 
     drawLines(A, B, vinfo, finfo, fbp);
     drawLines(B, C, vinfo, finfo, fbp);
@@ -520,7 +533,16 @@ void drawCannon(struct fb_var_screeninfo vinfo, struct fb_fix_screeninfo finfo, 
 
 }
 
-
+// unsigned char isShot(struct Point bullet, int shipOffsetX) {
+//     if (
+//         bullet.y <= (251 * HEIGHT / 1080) && 
+//         (shipOffsetX + 20 * WIDTH / 1920) <= (960 * WIDTH / 1920) && 
+//         (shipOffsetX + 100 * WIDTH / 1920) >= (960 * WIDTH / 1920)
+//     ) {
+//         return 1;
+//     }
+//     return 0;
+// }
 
 int main(void)
 {
@@ -529,7 +551,7 @@ int main(void)
     struct fb_fix_screeninfo finfo;
     long int screensize = 0;
     char *fbp = 0;
-    int x = 0, y = 0;
+    int x, y;
     long int location = 0;
 
     // Open the file for reading and writing
@@ -568,14 +590,14 @@ int main(void)
     int direction = 1;
 
     struct Point offset;
-    offset.x = 960*width/1920;
-    offset.y = 890*height/1080;
+    offset.x = 960 * WIDTH / 1920;
+    offset.y = 890 * HEIGHT / 1080;
 
     x = 0; y = 0;       // Where we are going to put the pixel
 
-    while( y < height-1 ) {
+    while( y < HEIGHT - 1 ) {
         x = 0;
-        while( x < width-1 ) {
+        while( x < WIDTH - 1 ) {
             location = (x + vinfo.xoffset) * (vinfo.bits_per_pixel / 8) +
                     (y+vinfo.yoffset) * finfo.line_length;
 
@@ -598,10 +620,12 @@ int main(void)
     noecho();
     nodelay(stdscr, TRUE);
     scrollok(stdscr, TRUE);
-    int xoffset = 30*width/1920;
+   
+    int xoffset = 30 * WIDTH / 1920;
+   
     while(1) {        
         drawCannon(vinfo, finfo, fbp);    
-        if(offset.y <= 251*height/1080 && (xoffset+20*width/1920<=960*width/1920 && xoffset+100*width/1920>=960*width/1920)) {
+        if(offset.y <= (251 * HEIGHT / 1080) && ((xoffset + 20 * WIDTH / 1920) <= (960 * WIDTH / 1920) && (xoffset + 100 * WIDTH / 1920) >= (960 * WIDTH / 1920))) {
             drawBullet(offset, vinfo, finfo, fbp, 0);
             clearShot(vinfo, finfo, fbp);
             while(1)
@@ -625,38 +649,38 @@ int main(void)
         }
         if(shot == 1) {
             drawBullet(offset, vinfo, finfo, fbp, 0);
-            offset.y -= 10*height/1080;
+            offset.y -= 10 * HEIGHT / 1080;
         }
         else if(shot == 2) {
             drawBullet(offset, vinfo, finfo, fbp, 1);
-            offset.y -= 10*height/1080;
-            offset.x -= 10*height/1080;
+            offset.y -= 10 * HEIGHT / 1080;
+            offset.x -= 10 * HEIGHT / 1080;
         }
         else if(shot == 3) {
             drawBullet(offset, vinfo, finfo, fbp, 2);
-            offset.y -= 10*height/1080;
-            offset.x += 10*height/1080;
+            offset.y -= 10 * HEIGHT / 1080;
+            offset.x += 10 * HEIGHT / 1080;
         }
         usleep(50000);
         clearShip(vinfo, finfo, fbp);
         clearShot(vinfo, finfo, fbp);
         if(direction) {
-            xoffset += 5*width/1920;
+            xoffset += 5 * WIDTH / 1920;
         } else {
-            xoffset -= 5*width/1920;
+            xoffset -= 5 * WIDTH / 1920;
         }
 
-        if(xoffset < 30*width/1920) {
+        if(xoffset < 30 * WIDTH / 1920) {
             direction = 1;
         }
 
-        if(xoffset > 1760*width/1920) {
+        if(xoffset > 1760 * WIDTH / 1920) {
             direction = 0;
         }
 
         if((offset.y < 1) || (offset.x < 1)) {
-            offset.y = 890*height/1080;
-            offset.x = 960*width/1920;
+            offset.y = 890 * HEIGHT / 1080;
+            offset.x = 960 * WIDTH / 1920;
             shot = 0;
         }
     }
